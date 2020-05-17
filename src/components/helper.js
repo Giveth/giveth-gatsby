72d2add7f7adf2284c4d299bec60d4735d8bd70b
James Farrell <info@jamespfarrell.com>
2020-05-17 16:57:26 +0200

    add giveth logo

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3'
 import Torus from '@toruslabs/torus-embed'
+import DirectWebSdk from '@toruslabs/torus-direct-web-sdk'
+
+function getTorus (direct) {
+  if (direct)
+    return new DirectWebSdk({
+      baseUrl: 'http://localhost:3000/serviceworker/',
+      GOOGLE_CLIENT_ID: 'MY CLIENT ID GOOGLE',
+      proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
+      network: 'ropsten' // details for test net
+    })
+  else return new Torus({ buttonPosition: 'top-right' })
+}
 
 const web3Obj = {
   web3: new Web3(),
@@ -9,8 +21,8 @@ const web3Obj = {
     web3Obj.web3 = web3Inst
   },
   initialize: async function (config) {
-    const torus = new Torus()
-    console.log(`config : ${JSON.stringify(config)}`)
+    const torus = getTorus(false)
+    console.log(`config1 : ${JSON.stringify(config)}`)
 
     const buildEnv = config.buildEnv || 'production'
     const networkConfig = {}
@@ -28,6 +40,16 @@ const web3Obj = {
     web3Obj.setweb3(torus.provider)
     web3Obj.torus = torus
     sessionStorage.setItem('pageUsingTorus', buildEnv)
+    const userInfo = await torus.getUserInfo()
+
+    console.log(`userInfo : ${JSON.stringify(userInfo, null, 2)}`)
+
+    const publicAddress = await torus.getPublicAddress({
+      verifier: userInfo.verifier,
+      verifierId: userInfo.email
+    })
+
+    console.log(`publicAddress ---> : ${publicAddress}`)
   }
 }
 export default web3Obj
