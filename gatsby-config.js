4c2266b9c5d633ae2088fc290ebb880b49262cfc
James Farrell <info@jamespfarrell.com>
2020-05-27 11:20:41 +0200

    current status

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
+global.Buffer = global.Buffer || require('buffer').Buffer
+
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str).toString('base64')
+  }
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString()
+  }
+}
+
 module.exports = {
   siteMetadata: {
     title: `Giveth Tor.us`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import web3Obj from '../components/helper'
+import { Grid, Box } from 'theme-ui'
 
 //xdai
 //rinkeby
@@ -24,8 +25,67 @@ web3Obj.initialize(xdaiConfig)
 
 const IndexPage = () => (
   <Layout>
-    <SEO title='Home' />
-    <h1>Giveth Tor.us</h1>
+    <div
+      sx={{
+        // applies width 100% to all viewport widths,
+        // width 50% above the first breakpoint,
+        // and 25% above the next breakpoint
+        width: ['100%', '50%', '25%']
+      }}
+    >
+      <SEO title='Home' />
+      <h1>Giveth Tor.us</h1>
+      <Grid width={[128, null, 192]}>
+        <Box bg='primary'>
+          Giveth DAC
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              web3Obj.torus.showWallet('transfer')
+            }}
+          >
+            donate
+          </button>
+        </Box>
+        <Box bg='muted'>
+          Aragon DAC
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              web3Obj.torus.showWallet('transfer')
+            }}
+          >
+            donate
+          </button>
+        </Box>
+        <Box bg='primary'>
+          Open source blockchain explorer
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              web3Obj.torus.showWallet('transfer')
+            }}
+          >
+            donate
+          </button>
+        </Box>
+        <Box bg='muted'>
+          Unicorn DAC
+          <br />
+          <br />
+          <button
+            onClick={() => {
+              web3Obj.torus.showWallet('transfer')
+            }}
+          >
+            donate
+          </button>
+        </Box>
+      </Grid>
+    </div>
   </Layout>
 )
 
