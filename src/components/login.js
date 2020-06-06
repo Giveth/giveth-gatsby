aae95bdfb0cbab251dfa91387536017317b64e5e
James Farrell <info@jamespfarrell.com>
2020-06-06 23:05:37 +0200

    Update verifier name

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
-import React from "react"
-import DirectWebSdk from "@toruslabs/torus-direct-web-sdk"
+import React from 'react'
+import DirectWebSdk from '@toruslabs/torus-direct-web-sdk'
 
 console.log(
   `process.env.GOOGLE_CLIENT_ID ---> : ${process.env.GATSBY_GOOGLE_CLIENT_ID}`
@@ -9,25 +9,25 @@ const torus = new DirectWebSdk({
   GOOGLE_CLIENT_ID: process.env.GATSBY_GOOGLE_CLIENT_ID,
   proxyContractAddress: process.env.GATSBY_PROXY_CONTRACT_ADDRESS,
   network: process.env.GATSBY_NETWORK,
-  enableLogging: true,
+  enableLogging: true
 })
 
-async function initTorus() {
+async function initTorus () {
   await torus.init()
 }
 
-async function login() {
-  console.log("in login")
+async function login () {
+  console.log('in login')
 
   await initTorus()
 
-  const userInfo = await torus.triggerLogin("google", "google-giveth")
+  const userInfo = await torus.triggerLogin('google', 'google-giveth2')
 
   console.log(`userInfo : ${JSON.stringify(userInfo, null, 2)}`)
 }
 
 const Login = () => (
-  <button style={{ right: 0, position: "absolute" }} onClick={login}>
+  <button style={{ right: 0, position: 'absolute' }} onClick={login}>
     Log in with tor.us
   </button>
 )
