ffe0a59aea6cbab5917ba0fb60c08310ffdd33e0
geleeroyale <krassvs@gmail.com>
2020-07-24 21:23:24 +0200

    add gatsby plugins for medium and mailchimp subscription - NEW ENV VARIABLE FOR MAILCHIMP needs to be set

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ if (typeof atob === 'undefined') {
 
 module.exports = {
   siteMetadata: {
-    title: 'Giveth Tor.us',
+    title: 'Giveth Simple Donation Application',
     description: 'The future of giving',
     author: '@giveth'
   },
@@ -31,6 +31,19 @@ module.exports = {
       options: {
         name: 'images',
         path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: 'gatsby-source-medium',
+      options: {
+        username: 'giveth'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: process.env.GATSBY_MAILCHIMP_APILINK, // string; add your MC list endpoint here; see instructions below
+        timeout: 3500 // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       }
     },
     'gatsby-transformer-sharp',
