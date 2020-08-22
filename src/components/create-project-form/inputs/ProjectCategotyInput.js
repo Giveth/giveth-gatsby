4433f3b47f9d0c44d9e416fe6f2b380416ead104
Bryan Mutai <mutaiwork@gmail.com>
2020-08-22 19:20:53 +0300

    feat - Create ProjectCategoryInput component
    for the create project flow and will be animated into the CreateProjectForm

---
+++ src/components/create-project-form/inputs/ProjectCategotyInput.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Checkbox, Label, Box, Text } from 'theme-ui'
+import { animated } from 'react-spring'
+
+export const ProjectCategoryInput = ({
+  register,
+  currentValue,
+  categoryList,
+  animationStyle
+}) => {
+  return (
+    <animated.section style={{ ...animationStyle, marginTop: '30px' }}>
+      <Label
+        sx={{
+          fontSize: 8,
+          fontFamily: 'heading'
+        }}
+        htmlFor='projectDescription'
+      >
+        Please select a category
+      </Label>
+      <Text
+        sx={{
+          fontSize: '3',
+          fontFamily: 'heading',
+          color: 'secondary',
+          mt: '8px',
+          lineHeight: '19px'
+        }}
+      >
+        You can select multiple categories
+      </Text>
+      <Box sx={{ mt: '50px' }}>
+        {categoryList.map(category => {
+          return (
+            <Label
+              sx={{ mb: '10px', display: 'flex', alignItems: 'center' }}
+              key={`${category.name}-label`}
+            >
+              <Checkbox
+                key={`${category.name}-checkbox`}
+                id={category.name}
+                name={category.name}
+                ref={register}
+                defaultChecked={
+                  currentValue
+                    ? currentValue[category.name][0] === 'on'
+                      ? 1
+                      : 0
+                    : 0
+                }
+              />
+              <Text sx={{ fontFamily: 'body' }}>{category.value}</Text>
+            </Label>
+          )
+        })}
+      </Box>
+    </animated.section>
+  )
+}
