2e473fa21e6537e3c9bce724275de54b82cf9e69
geleeroyale <krassvs@gmail.com>
2020-10-28 22:04:33 +0100

    ADD DEPENDENCY: contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID || '',
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ''
@@ -64,7 +64,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/giveth-logo-blue.svg' // This path is relative to the root of the site.
       }
     },
     {
