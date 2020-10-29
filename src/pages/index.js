a7c28027c7da6c80052efabb5967ae065c604a9b
geleeroyale <krassvs@gmail.com>
2020-10-29 09:22:10 +0100

    correct merge conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import HomeTopProjects from '../components/HomeTopProjects'
 const IndexPage = () => {
   return (
     <Layout isHomePage='true'>
+      <SEO title='home />'
       <Hero />
       <HomeTopProjects />
       <InfoSection />
