0db19c9caadb0976f2e5bf1272d6a21a5eb56c8f
geleeroyale <krassvs@gmail.com>
2020-12-30 15:30:40 +0100

    cleaner category display

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const categoryList = [
   { name: 'poverty', value: 'End Poverty' },
   { name: 'hunger', value: 'Zero Hunger' },
   { name: 'health', value: 'Good Health and Well-being' },
-  { name: 'education', value: 'Good Health and Well-being' },
+  { name: 'education', value: 'Quality Education' },
   { name: 'gender-equality', value: 'Gender Equality' },
   { name: 'clean-water', value: 'Clean Water and Sanitation' },
   { name: 'energy', value: 'Affordable and Clean Energy' },
@@ -22,7 +22,7 @@ export const categoryList = [
   },
   { name: 'climate-action', value: 'Climate Action' },
   { name: 'life-water', value: 'Life Below Water' },
-  { name: 'life-land', value: 'Life Below Water' },
+  { name: 'life-land', value: 'Life On Land' },
   { name: 'justice', value: 'Peace and Justice Strong Institutions' },
   { name: 'partnership  ', value: 'Partnerships to achieve the Goal' },
   { name: 'other', value: 'Other' }
