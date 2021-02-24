c7ca4c0f6873cfdf7d10000b910a4ce29962ae0c
James Farrell <info@jamespfarrell.com>
2021-02-24 20:56:01 +0100

    debug upload

--- src/components/project/updatesTab.js
+++ src/components/project/updatesTab.js
@@ -35,21 +35,17 @@ const UpdatesTab = ({ showModal, setShowModal, project, isOwner }) => {
     try {
       if (!title || !content)
         return Toast({ content: 'Fields should not be empty', type: 'error' })
+      console.log('Remove the refetch')
       const { data } = await addUpdateMutation({
         variables: {
           projectId: parseFloat(project?.id),
           title,
           content
-        },
-        refetchQueries: [
-          {
-            query: GET_PROJECT_UPDATES,
-            variables: { projectId: parseInt(project?.id), take: 100, skip: 0 }
-          }
-        ],
-        awaitRefetchQueries: true
+        }
       })
     } catch (error) {
+      console.error('addUpdate')
+
       console.log({ error })
       alert(JSON.stringify(error?.message))
     }
