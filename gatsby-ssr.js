0b096e7f9cd2492a6dc1944339d285ddcdf957b5
geleeroyale <krassvs@gmail.com>
2021-04-28 15:38:32 +0200

    use giveth support channel, cleanup

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,22 +3,29 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-import React, { useState } from 'react'
+import React from 'react'
 
-export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+import styled from '@emotion/styled'
+
+const Crate = styled.div`
+  position: fixed;
+  z-index: 100;
+  bottom: 0px;
+`
+
+export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <>
+    <Crate>
       <widgetbot
         server='679428761438912522'
         channel='835168432520429578'
         width='800'
         height='600'
         shard='https://emerald.widgetbot.io'
-        id='widgetbot'
       ></widgetbot>
 
       <script src='https://cdn.jsdelivr.net/npm/@widgetbot/html-embed'></script>
-    </>
+    </Crate>
   ])
 }
 
